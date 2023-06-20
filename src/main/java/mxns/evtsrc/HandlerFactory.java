16c4fde0c714985e962b5a618f585830b0b9671d
Måns Rolandi <mxns@users.noreply.github.com>
2023-06-20 14:14:19 +0200

    add handler return type

--- src/main/java/mxns/evtsrc/HandlerFactory.java
+++ src/main/java/mxns/evtsrc/HandlerFactory.java
@@ -1,35 +1,43 @@
 package mxns.evtsrc;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
- * @param <I> Input
- * @param <C> Context
- * @param <O> Output
+ * @param <I> Input type
+ * @param <C> Context type
+ * @param <R> Handler return type
+ * @param <O> Output type
  */
-public class HandlerFactory<I, C, O> {
-    private final BiFunction<I, Function<C, CompletableFuture<O>>, CompletableFuture<O>> contextCreator;
+public class HandlerFactory<I, C, R, O> {
+    private final BiFunction<I, Function<C, CompletableFuture<O>>, CompletableFuture<O>> deserializer;
+    private final Function<R, CompletableFuture<O>> serializer;
     private final BiFunction<C, Throwable, CompletableFuture<O>> exceptionHandler;
+    private final Executor executor;
 
     public HandlerFactory(
-            BiFunction<I, Function<C, CompletableFuture<O>>, CompletableFuture<O>> contextCreator,
-            BiFunction<C, Throwable, CompletableFuture<O>> exceptionHandler
+            BiFunction<I, Function<C, CompletableFuture<O>>, CompletableFuture<O>> deserializer,
+            Function<R, CompletableFuture<O>> serializer,
+            BiFunction<C, Throwable, CompletableFuture<O>> exceptionHandler,
+            Executor executor
     ) {
-        this.contextCreator = contextCreator;
+        this.deserializer = deserializer;
+        this.serializer = serializer;
         this.exceptionHandler = exceptionHandler;
+        this.executor = executor;
     }
 
-    public Function<I, CompletableFuture<O>> createHandler(Function<C, Supplier<CompletableFuture<O>>> handlerFactory) {
-        return input -> contextCreator.apply(input, context -> apply(handlerFactory, context));
+    public Function<I, CompletableFuture<O>> createHandler(Function<C, Supplier<CompletableFuture<R>>> factory) {
+        return input -> deserializer.apply(input, context -> apply(factory, context));
     }
 
-    private CompletableFuture<O> apply(Function<C, Supplier<CompletableFuture<O>>> handlerFactory, C context) {
+    private CompletableFuture<O> apply(Function<C, Supplier<CompletableFuture<R>>> factory, C context) {
         CompletableFuture<O> future;
         try {
-            future = handlerFactory.apply(context).get();
+            future = factory.apply(context).get().thenComposeAsync(serializer, executor);
         } catch (Exception e) {
             return handleException(context, e);
         }
@@ -39,7 +47,7 @@ public class HandlerFactory<I, C, O> {
     private CompletableFuture<O> handleException(C context, Throwable throwable) {
         try {
             return exceptionHandler.apply(context, throwable)
-                    .thenCompose(output -> CompletableFuture.failedFuture(new IgnorableException(throwable)));
+                    .thenComposeAsync(output -> CompletableFuture.failedFuture(new IgnorableException(throwable)), executor);
         } catch (Exception e) {
             return CompletableFuture.failedFuture(e);
         }
--- src/test/java/mxns/evtsrc/HandlerFactoryTest.java
+++ src/test/java/mxns/evtsrc/HandlerFactoryTest.java
@@ -6,31 +6,46 @@ import org.junit.Test;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class HandlerFactoryTest {
     @Test
-    public void test() throws ExecutionException, InterruptedException {
+    public void shouldCompleteSuccessfully() throws ExecutionException, InterruptedException {
         BiFunction<Map<String, String>, Function<Context, CompletableFuture<String>>, CompletableFuture<String>> contextCreator = (data, f) -> f.apply(new Context(data.get("apa")));
         BiFunction<Context, Throwable, CompletableFuture<String>> exceptionHandler = (context, throwable) -> {
             System.out.println("I got this: " + throwable.getMessage());
             return CompletableFuture.completedFuture("handled exception");
         };
-        BiFunction<Context, Throwable, CompletableFuture<String>> failedException = (context, throwable) -> {
-            //return CompletableFuture.failedFuture(new RuntimeException("Failure in exception handler"));
-            throw new RuntimeException("Failure in exception handler");
-        };
-        HandlerFactory<Map<String, String>, Context, String> factory = new HandlerFactory<>(contextCreator, exceptionHandler);
+        Executor executor = Runnable::run;
+        HandlerFactory<Map<String, String>, Context, String, String> factory = new HandlerFactory<>(contextCreator, CompletableFuture::completedFuture, exceptionHandler, executor);
         Function<Context, Supplier<CompletableFuture<String>>> function = context -> () -> CompletableFuture.completedFuture(context.value);
-        Function<Context, Supplier<CompletableFuture<String>>> failed = context -> () -> {
-            return CompletableFuture.failedFuture(new RuntimeException("Failure"));
-            //throw new RuntimeException("Failure");
-        };
         Function<Map<String, String>, CompletableFuture<String>> handler = factory.createHandler(function);
         CompletableFuture<String> apply = handler.apply(Map.of("apa", "giraff"));
         Assert.assertEquals("giraff", apply.get());
+    }
+
+    @Test(expected = IgnorableException.class)
+    public void shouldFail() throws Throwable {
+        BiFunction<Map<String, String>, Function<Context, CompletableFuture<String>>, CompletableFuture<String>> contextCreator = (data, f) -> f.apply(new Context(data.get("apa")));
+        BiFunction<Context, Throwable, CompletableFuture<String>> exceptionHandler = (context, throwable) -> {
+            System.out.println("I got this: " + throwable.getMessage());
+            return CompletableFuture.completedFuture("handled exception");
+        };
+        Executor executor = Runnable::run;
+        HandlerFactory<Map<String, String>, Context, String, String> factory = new HandlerFactory<>(contextCreator, CompletableFuture::completedFuture, exceptionHandler, executor);
+        Function<Context, Supplier<CompletableFuture<String>>> function = context -> () -> {
+            throw new RuntimeException("Failure");
+        };
+        Function<Map<String, String>, CompletableFuture<String>> handler = factory.createHandler(function);
+        CompletableFuture<String> apply = handler.apply(Map.of("apa", "giraff"));
+        try {
+            apply.get();
+        } catch (ExecutionException e) {
+            throw e.getCause();
+        }
     }
 
     static class Context {
